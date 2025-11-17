20599dc13840eedd24c6291800441add20511368
Melton Classic <saadjameel11000@gmail.com>
2025-11-17 21:06:33 +0500

    contact-page

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,5 +91,64 @@ $(document).ready(function () {
       if (val > 1) input.val(val - 1);
     });
   });
+
+  // Shipping Cart
+  $(document).ready(function () {
+    function updateCartTotal() {
+      var subtotal = 0;
+      $('.cart-item').each(function () {
+        var $item = $(this);
+        var price = parseFloat($item.data('price'));
+        var quantity = parseInt($item.find('.qty-input').val());
+        subtotal += price * quantity;
+      });
+      var shipping = 0;
+      var total = subtotal + shipping;
+      $('.subtotal-amount').text('$' + subtotal.toFixed(2));
+      $('.total-amount').text('$' + total.toFixed(2));
+    }
+    $('.qty-plus').on('click', function () {
+      var $input = $(this).siblings('.qty-input');
+      var currentValue = parseInt($input.val());
+      $input.val(currentValue + 1);
+      updateCartTotal();
+    });
+    $('.qty-minus').on('click', function () {
+      var $input = $(this).siblings('.qty-input');
+      var currentValue = parseInt($input.val());
+      var minValue = parseInt($input.attr('min'));
+
+      if (currentValue > minValue) {
+        $input.val(currentValue - 1);
+        updateCartTotal();
+      }
+    });
+    $('.qty-input').on('change', function () {
+      var $input = $(this);
+      var currentValue = parseInt($input.val());
+      var minValue = parseInt($input.attr('min'));
+
+      if (isNaN(currentValue) || currentValue < minValue) {
+        $input.val(minValue);
+      }
+      updateCartTotal();
+    });
+    $('.remove-item').on('click', function () {
+      $(this).closest('.cart-item').remove();
+      updateCartTotal();
+    });
+    updateCartTotal();
+    $('#applyCoupon').on('click', function () {
+      var code = $('#couponCode').val();
+      if (code) {
+        alert('Coupon Code "' + code + '" lagane ki koshish ki ja rahi hai.');
+      } else {
+        alert('Meherbaani kar ke coupon code daalen.');
+      }
+    });
+    $('.checkout-button').on('click', function () {
+      alert('Aap checkout page par ja rahe hain.');
+    });
+  });
 });
 
